b4eb855e2b76eec84d436e9778545552a33d9b6c
olajczuz <olajczuz@fjfi.cvut.cz>
2018-10-06 19:10:29 +0200

    cv1 & cv2 code added

---
+++ cv1/helloWorld1.cpp
@@ -0,0 +1,7 @@
+1 // my first program in C++
+2 #include <iostream>
+3
+4 int main()
+5 {
+6  std::cout << "Hello World!";
+7 }
---
+++ cv2/podmienky&cykly.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int addition (int a, int b)
+{
+    int r;
+    r=a+b;
+    return r;
+}
+
+int subtraction (int a, int b)
+{
+    int r;
+    r=a-b;
+    return r;
+}
+
+//priklad funkcie ktora nevracia ziadnu hodnotu iba nieco vypisuje
+void printmessage ()
+{
+    cout << "I'm a function!";
+}
+
+int main ()
+{
+    //prvy priklad
+
+    int z;
+    z = addition (5,3);
+    cout << "The result of 5 + 3 is " << z << '\n';
+    cout << '\n';
+
+    //druhy priklad
+    int x=5, y=3;
+    z = subtraction (7,2);
+    cout << "7 - 2 is " << z << '\n';
+    cout << "I can get " << subtraction (7,2) <<  " also like this" << '\n';
+    cout << "5 - 3 is " << subtraction (x,y) <<  '\n';
+    z= 4 + subtraction (x,y);
+    cout << "4 + 5 - 3 is " << z << '\n';
+    cout << '\n';
+
+    //treti priklad
+    printmessage ();
+    cout << '\n';
+
+}
+
