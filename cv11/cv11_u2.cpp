51dcada9b3d1b3623f030a5d67d2cde87fea6655
olajczuz <olajczuz@fjfi.cvut.cz>
2018-11-06 08:01:26 +0100

    cv11

---
+++ cv11/cv11_u2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+    int *ptr1, *ptr2;
+
+    ptr1 = /*doplnte*/; // ptr1 stores the address of num1
+    ptr2 = /*doplnte*/; // ptr2 stores the address of num2
+
+
+    printf("Enter number 1: ");
+    cin >> /*doplnte*/ ;
+
+    printf("Enter number 2: ");
+    cin >> /*doplnte*/;
+
+    sum = *ptr1 + *ptr2;
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
